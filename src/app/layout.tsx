65f5b70d0da5c938b9874b7e7fdc7da0e83e6912
ckuzmick <conrad.kuzmick@icloud.com>
2024-11-23 12:42:50 -0500

    stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "@/styles/globals.css";
 import { Cormorant_Garamond } from "next/font/google";
+import Header from "@/components/header";
 
 const cormorantGaramond = Cormorant_Garamond({
   weight: "400",
@@ -23,6 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${cormorantGaramond.variable}`}>
+        <Header/>
         {children}
       </body>
     </html>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,17 @@
-// import Image from 'next/image';
-import "@/styles/header.css";
+import Image from 'next/image';
+import "@/styles/Header.module.css";
 
 export default function Header() {
     return (
         <header>
-            {/* <Image
-                src="/rf-banner.png"
+            <Image
+                src="/rf-banner.svg"
                 alt="logo"
                 className="image"
                 width={900}
                 height={100}
                 loading="eager"
-            /> */}
+            />
         </header>
     );
 }
