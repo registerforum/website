55cf7eccaa178c665957e40be7c119aa5fd2e791
ckuzmick <conrad.kuzmick@icloud.com>
2024-11-27 09:47:37 -0500

    section

--- src/app/section/[slug]/page.js
+++ src/app/section/[slug]/page.js
@@ -1,106 +1,79 @@
 import { google } from "googleapis";
+import styles from "@/styles/Article.module.css";
 
 // Next.js will revalidate the cache at most once every 60 seconds
-export const revalidate = 3660;
+export const revalidate = 3600;
 
 // Allow dynamic parameters for server-side rendering on unknown paths
 export const dynamicParams = true;
 
-const sheetId = process.env.SHEET_ID;
-const keys = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+// Fetch articles from Google Sheets
+async function fetchArticles() {
+  const sheetId = process.env.SHEET_ID;
+  const keys = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 
-// Fetch sections from Google Sheets
-async function fetchSections() {
-  try {
-    const auth = await google.auth.getClient({
-      projectId: keys.project_id,
-      credentials: {
-        type: "service_account",
-        private_key: keys.private_key,
-        client_email: keys.client_email,
-        client_id: keys.client_id,
-        token_url: keys.token_uri,
-        universe_domain: "googleapis.com",
-      },
-      scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-    });
+  const auth = await google.auth.getClient({
+    projectId: keys.project_id,
+    credentials: {
+      type: "service_account",
+      private_key: keys.private_key,
+      client_email: keys.client_email,
+    },
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+  });
 
-    const sheets = google.sheets({ version: "v4", auth });
+  const sheets = google.sheets({ version: "v4", auth });
 
-    const response = await sheets.spreadsheets.values.get({
-      spreadsheetId: sheetId,
-      range: `Sections!A2:E22`, // Adjust range as needed
-    });
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId: sheetId,
+    range: `Articles!A2:N`, // Adjust the range as needed
+  });
 
-    const rows = response.data.values || [];
+  const rows = response.data.values || [];
 
-    const sectionsMap = {};
-
-    rows.forEach(row => {
-      const [name, editorsStr, type, parentName, slug] = row;
-      const editors = editorsStr ? editorsStr.split(", ").map((editor) => editor.trim()) : [];
-
-      if (type === "parent") {
-        sectionsMap[slug] = { 
-          Parent: { name, editors, type, parent: null, slug },
-          Children: []
-        };
-      } else if (type === "child" || type === "sub") {
-        const parent = sectionsMap[parentName];
-        if (parent) {
-          parent.Children.push({ name, editors, type, parent: parentName, slug });
-        }
-      }
-    });
-
-    const formattedData = Object.values(sectionsMap);
-
-    console.log("Fetched spreadsheet data.");
-
-    return formattedData;
-  } catch (error) {
-    console.error("Error fetching spreadsheet data:", error);
-    return [];
-  }
+  return rows.map((row) => ({
+    title: row[0] || null,
+    author: row[1] || null,
+    date: row[2] || null,
+    slug: row[4] || null,
+    // cover: row[12] ? row[12].replace("https://drive.google.com/file/d/", "https://drive.usercontent.google.com/download?id=").slice(0, -18).concat("&export=view&authuser=0") || null : null,
+    cover: row[5] || null,
+    // views: parseInt(row[8]) || null,
+    // trending: row[9] === "TRUE",
+    type: row[10] || null,
+    body: row[11] || null,
+    caption: row[13] || null,
+  }));
 }
 
 // Generate static params for all articles
 export async function generateStaticParams() {
-  const sections = await fetchSections();
-  
-  const params = [];
+  const articles = await fetchArticles();
 
-  // Generate params for each parent and its children
-  sections.forEach(({ Parent }) => {
-    params.push({ params: { slug: Parent.slug } });
-  });
-
-  return params;
+  return articles.map((article) => ({
+    slug: article.slug,
+  }));
 }
 
 // Render the article page
 export default async function Page({ params: paramsPromise }) {
   const params = await paramsPromise;
-  const sections = await fetchSections();
-  
-  // Find the parent section based on the slug
-  const section = sections.find(section => section.Parent.slug === params.slug);
-
-  if (!section) {
-    // Handle case when section is not found, e.g., show a 404 page or a message
-    return <main><h1>Section not found</h1></main>;
-  }
+  const articles = await fetchArticles();
+  const article = articles.find((a) => a.slug === params.slug);
+  const pars = article.body.split("\n");
 
   return (
-    <main>
-      <h1>{section.Parent.name}</h1>
-      <ul>
-        {section.Children.map((child) => (
-          <li key={child.slug}>
-            <a href={`/section/${child.slug}`}>{child.name}</a>
-          </li>
+    <main className={styles.container}>
+      <h1 className={styles.title}>{article.title}</h1>
+      <div className={styles.cover}>
+        <img className={styles.image} src={article.cover} alt={article.title} />
+        <p className={styles.byline}>{article.caption}</p>
+      </div>
+      <article className={styles.body}>
+        {pars.map((par, index) => (
+          <p key={index} className={styles.par}>{par}</p>
         ))}
-      </ul>
+      </article>
     </main>
   );
 }
