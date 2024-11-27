7a1790c06f4fc6aaa8da760ae61b2a069edc100a
ckuzmick <conrad.kuzmick@icloud.com>
2024-11-27 10:24:14 -0500

    section

--- src/app/section/[slug]/page.js
+++ src/app/section/[slug]/page.js
@@ -1,69 +1,8 @@
-import { google } from "googleapis";
 import styles from "@/styles/Article.module.css";
+import fetchSections from "@/utils/sections";
 
 export const revalidate = 3600;
-
 export const dynamicParams = true;
-
-async function fetchSections() {
-  const sheetId = process.env.SHEET_ID;
-  const keys = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
-
-  const auth = await google.auth.getClient({
-    projectId: keys.project_id,
-    credentials: {
-      type: "service_account",
-      private_key: keys.private_key,
-      client_email: keys.client_email,
-    },
-    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-  });
-
-  const sheets = google.sheets({ version: "v4", auth });
-
-  const response = await sheets.spreadsheets.values.get({
-    spreadsheetId: sheetId,
-    range: `Sections!A2:N`,
-  });
-
-  const rows = response.data.values || [];
-
-  const sections = [];
-
-  for (const row of rows) {
-    if (row[2] === "parent" || row[2] === "child") {
-      sections.push({
-        name: row[0] || null,
-        editors: row[1] ? row[1].split(", ") : null,
-        type: row[2] || null,
-        parent: row[3] || null,
-        slug: row[4] || null,
-        children: [],
-      });
-    } else {
-      const parent = sections.find((a) => a.slug === row[3]);
-      if (parent) {
-        parent.children.push({
-          name: row[0] || null,
-          editors: row[1] ? row[1].split(", ") : null,
-          type: row[2] || null,
-          parent: row[3] || null,
-          slug: row[4] || null,
-        });
-      }
-    }
-  }
-
-  return sections;
-
-  // return rows.map((row) => ({
-  //   name: row[0] || null,
-  //   editors: row[1].split(", ") || null,  
-  //   type: row[2] || null,
-  //   parent: row[3] || null,
-  //   slug: row[4] || null,
-  // }));
-}
 
 export async function generateStaticParams() {
   const sections = await fetchSections();
