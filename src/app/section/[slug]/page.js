9a49d83f3779e8439acc3becae5550c646843825
ckuzmick <conrad.kuzmick@icloud.com>
2024-11-27 09:56:30 -0500

    section

--- src/app/section/[slug]/page.js
+++ src/app/section/[slug]/page.js
@@ -1,14 +1,12 @@
 import { google } from "googleapis";
 import styles from "@/styles/Article.module.css";
+import { type } from "os";
 
-// Next.js will revalidate the cache at most once every 60 seconds
 export const revalidate = 3600;
 
-// Allow dynamic parameters for server-side rendering on unknown paths
 export const dynamicParams = true;
 
-// Fetch articles from Google Sheets
-async function fetchArticles() {
+async function fetchSections() {
   const sheetId = process.env.SHEET_ID;
   const keys = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 
@@ -26,39 +24,36 @@ async function fetchArticles() {
 
   const response = await sheets.spreadsheets.values.get({
     spreadsheetId: sheetId,
-    range: `Sections!A2:N`, // Adjust the range as needed
+    range: `Sections!A2:N`,
   });
 
   const rows = response.data.values || [];
 
   return rows.map((row) => ({
-    title: row[0] || null,
-    author: row[1] || null,
-    date: row[2] || null,
+    name: row[0] || null,
+    editors: row[1].split(", ") || null,  
+    type: row[2] || null,
+    parent: row[3] || null,
     slug: row[4] || null,
-    // cover: row[12] ? row[12].replace("https://drive.google.com/file/d/", "https://drive.usercontent.google.com/download?id=").slice(0, -18).concat("&export=view&authuser=0") || null : null,
-    cover: row[5] || null,
   }));
 }
 
-// Generate static params for all articles
 export async function generateStaticParams() {
-  const articles = await fetchArticles();
+  const sections = await fetchSections();
 
-  return articles.map((article) => ({
-    slug: article.slug,
+  return sections.map((section) => ({
+    slug: section.slug,
   }));
 }
 
-// Render the article page
 export default async function Page({ params: paramsPromise }) {
   const params = await paramsPromise;
-  const articles = await fetchArticles();
-  const article = articles.find((a) => a.slug === params.slug);
+  const sections = await fetchSections();
+  const section = sections.find((a) => a.slug === params.slug);
 
   return (
     <main className={styles.container}>
-      <h1 className={styles.title}>{article.title}</h1>
+      <h1 className={styles.title}>{section.name}</h1>
 
     </main>
   );
