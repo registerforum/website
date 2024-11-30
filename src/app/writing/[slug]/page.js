2d96a7727c8b7f66ff71f248f562b068a9ba6b47
ckuzmick <conrad.kuzmick@icloud.com>
2024-11-29 21:19:45 -0500

    stuff

--- src/app/writing/[slug]/page.js
+++ src/app/writing/[slug]/page.js
@@ -36,7 +36,9 @@ export default async function Page({ params: paramsPromise }) {
         <img className={styles.image} src={article.cover} alt={article.title} />
         <p className={styles.caption}>{article.caption}</p>
       </div>
-      <a className={styles.author} href={`/staff/${article.author.toLowerCase().replace(/\s/g, "-")}`}>By&nbsp;{article.author},&nbsp;<div className={styles.position}>{author.position}</div></a>
+      {article.author && (
+        <a className={styles.author} href={`/staff/${article.author.toLowerCase().replace(/\s/g, "-")}`}>By&nbsp;{article.author},&nbsp;<div className={styles.position}>{author.position}</div></a>
+      )}
       <article className={styles.body}>
         {pars.map((par, index) => (
           <p key={index} className={styles.par}>{par}</p>
