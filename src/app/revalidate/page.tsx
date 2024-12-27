eeee8e4c52b54392b8f2bd3f4b88338638fad454
Conrad Kuzmick <ck@mini.lan>
2024-12-27 12:21:07 -0500

    stuff

--- src/app/revalidate/page.tsx
+++ src/app/revalidate/page.tsx
@@ -4,22 +4,43 @@ import { useState, useEffect } from 'react'
 
 const SlugPage = () => {
   const [loading, setLoading] = useState<boolean>(true)
+  const [message, setMessage] = useState<string | null>("Loading...")
 
   useEffect(() => {
     const fetchRevalidation = async () => {
       setLoading(true)
       try {
+        setMessage("Revalidating Pages...")
+
         const res = await fetch('/api/revalidate', {
           method: 'POST',
           headers: {
-            'Content-Type': 'application/json',
+        'Content-Type': 'application/json',
           },
           body: JSON.stringify({ path: '/writing/[slug]/page' }),
         })
 
         if (!res.ok) {
+          setMessage("Failed to revalidate")
           throw new Error('Failed to revalidate')
         }
+
+        setMessage("Revalidating Root...")
+
+        const resRoot = await fetch('/api/revalidate', {
+          method: 'POST',
+          headers: {
+        'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ path: '/' }),
+        })
+
+        if (!resRoot.ok) {
+          setMessage("Failed to revalidate root")
+          throw new Error('Failed to revalidate root')
+        }
+
+        setMessage("Revalidated")
       } catch (error) {
         console.error('Error fetching revalidation:', error)
       } finally {
@@ -30,13 +51,9 @@ const SlugPage = () => {
     fetchRevalidation()
   }, [])
 
-  if (loading) {
-    return <p>Loading...</p>
-  }
-
   return (
     <div>
-      <h1>Revalidated</h1>
+      <h1>{message}</h1>
     </div>
   )
 }
